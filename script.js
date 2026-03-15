253fbccf8859bc5d00c6e2e31ebec0920a12450d
Lucas Lozardo <lucas_losardo@hotmail.com>
2026-03-15 17:47:48 -0300

    refactor: btn using the alterarContexto() function

--- script.js
+++ script.js
@@ -3,25 +3,49 @@ const btnFoco = document.querySelector(".app__card-button--foco");
 const btnCurto = document.querySelector(".app__card-button--curto");
 const btnLongo = document.querySelector(".app__card-button--longo");
 const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
 
 const btn = document.querySelector(".app__card-button");
 
 btnFoco.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "foco");
-    banner.setAttribute("src", "/imagens/foco.png");
+    alterarContexto("foco");
+
     
 });
 
 btnCurto.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "descanso-curto");
-    banner.setAttribute("src", "/imagens/descanso-curto.png");
-    
+    alterarContexto("descanso-curto");
+     
 });
 
 btnLongo.addEventListener("click", () => {
-    banner.setAttribute("src", "/imagens/descanso-longo.png");
-    html.setAttribute("data-contexto", "descanso-longo");
+    alterarContexto("descanso-longo");
 });
 
 
+function alterarContexto(contexto){
+    html.setAttribute("data-contexto", contexto);
+    banner.setAttribute("src", `/imagens/${contexto}.png`);
+    
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            break;
 
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`
+                break;
+
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar a superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`
+            break;
+        
+        default:
+            break;
+    }
+}
+
+const pg = document.getElementById
